3e472d43a844d7cccf25d03f135513c814ccf35b
Rajendra214 <rajendraariellasatriani@gmail.com>
2024-11-04 16:16:27 +0700

    Add files via upload

---
+++ tugas9.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NITRO V 15
+ */
+public class tugas9 {
+
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama depan anda untuk di eja : ");
+        String nama = scanner.nextLine();
+
+        System.out.println("\nEjaan untuk \"" + nama + "\" adalah :");
+
+        for (int i = 0; i < nama.length(); i++) {
+            System.out.println("Huruf ke-" + (i + 1) + " : " + nama.charAt(i));
+        }
+    }
+}
+
+
